bf78b3ad0f3718ce1cd580113a11f6ff64a15edb
massimo_rocca <massimo_rocca@hotmail.com>
2021-10-21 23:26:38 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BTN_1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BTN_1
+        '
+        Me.BTN_1.Location = New System.Drawing.Point(280, 179)
+        Me.BTN_1.Name = "BTN_1"
+        Me.BTN_1.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_1.TabIndex = 0
+        Me.BTN_1.Text = "Press Me"
+        Me.BTN_1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.BTN_1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents BTN_1 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles BTN_1.Click
+        MessageBox.Show("hello world !")
+    End Sub
+End Class
